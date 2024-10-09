1a29b5d265138c9d4cce3d9823c7951aa094f8f0
POLAR <3398993264@qq.com>
2024-10-09 21:38:13 +0800

    final

---
+++ hw2/LinearRegression.py
@@ -0,0 +1,9 @@
+import numpy as np  
+import matplotlib.pyplot as plt
+from dataset import generate_linear_data, add_bias
+from plotting import plot_results
+from linear_regression import LinearRegression
+
+if __name__ == "__main__":
+    # 使用噪声数据集
+   
